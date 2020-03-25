827980058dba891c73ce85a53316114e7d30e4a4
Victor Onuchukwu <chisomonuchukwu91@gmail.com>
2020-03-26 00:14:54 +0100

    first commit

---
+++ app/src/main/java/com/example/javaapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.javaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
